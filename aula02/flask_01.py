7c638a2b002f0ef5144343dffd8ba576bc621d79
Luiz Ferrarezi <lferrarezi@gmail.com>
2018-05-26 11:13:05 -0300

    aula 02

--- aula02/flask_01.py
+++ aula02/flask_01.py
@@ -1,10 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 
 app = Flask(__name__) # main
 
 @app.route('/') # o app estanciado como flask está ouvindo o acesso na raiz (/)
 def index(): # esta é a função a ser executada quando do acesso na raiz (/)
-    return 'Hello World'
+    retorno = {'status':1 ,
+               'mensagem':'Hello World!'}
+    return jsonify(retorno)
 
-# app.run(host='0.0.0.0')
-app.run()
+@app.route('/a/') # o app estanciado como flask está ouvindo o acesso em a (/a)
+@app.route('/a') # o app estanciado como flask está ouvindo o acesso em a (/a)
+def a(): # esta é a função a ser executada quando do acesso em a (/a)
+    return jsonify([{'Gasolina': 'O Preço da gasolina...'},
+                    {'Etanol':'Acabou!'},
+                    {'Mensagem':'Halla Warld!'}
+                    ])
+
+@app.route('/teste')
+def teste():
+    return jsonify([{'Mensagem':'Teste!'}
+                    ])
+
+
+# app.run(host='0.0.0.0', debug=True, port='80')
+app.run(debug=True)
